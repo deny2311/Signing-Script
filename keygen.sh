b053a606d9c4a12ef79c4237496d8547664c15cb
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-06-26 00:01:08 +0530

    Update keygen.sh

--- keygen.sh
+++ keygen.sh
@@ -17,18 +17,32 @@ if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
 fi
 clear
 
+# Check for existing Android certs and prompt for removal
+if [ -d "$HOME/.android-certs" ]; then
+    read -p "Existing Android certificates found. Do you want to remove them? (y/n): " remove_confirmation
+    if [[ $remove_confirmation == "y" || $remove_confirmation == "Y" ]]; then
+        rm -rf "$HOME/.android-certs"
+        echo "Old Android certificates removed."
+    else
+        echo "Exiting without changes."
+        exit 1
+    fi
+fi
+
 # Create Key
 echo "Press ENTER TWICE to skip password (about 10-15 enter hits total). Cannot use a password for inline signing!"
 mkdir ~/.android-certs
 
-for x in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot; do \
-    ./development/tools/make_key ~/.android-certs/$x "$subject"; \
+for x in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot; do 
+    ./development/tools/make_key ~/.android-certs/$x "$subject"
 done
 
-## Create vendor for keys
-mkdir vendor/lineage-priv
+# Create vendor directory for keys
+mkdir -p vendor/lineage-priv
 mv ~/.android-certs vendor/lineage-priv/keys
 echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
+
+# Create BUILD.bazel file
 cat <<EOF > vendor/lineage-priv/keys/BUILD.bazel
 filegroup(
     name = "android_certificate_directory",
